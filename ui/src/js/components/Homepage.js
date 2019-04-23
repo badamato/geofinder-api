4672439cd559eadc7354b516112f2833bae4fb28
Beth Damato <bethadamato@gmail.com>
2019-04-23 17:53:45 -0400

    adds styling

--- ui/src/js/components/Homepage.js
+++ ui/src/js/components/Homepage.js
@@ -1,16 +1,29 @@
 import React, {PureComponent} from 'react';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import SearchIcon from '@material-ui/icons/Search'
+import SearchIcon from '@material-ui/icons/Search';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 import style from '../../css/style.css';
+
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none',
+    },
+});
 
 
 class HomePage extends PureComponent {
 
     render() {
+        const { classes } = this.props;
 
         return (
         <div className='container'>
@@ -24,6 +37,18 @@ class HomePage extends PureComponent {
                     //value={ this.props.currentTransaction }
                     //onChange={(e) => this.props.updateCurrentTransaction("currentTransaction", e.target.value) }
                     className='inputInput'
+                />
+                </div>
+                <div>
+                    <Button variant="contained" color="primary" size="small" className={classes.button}>
+                        GO
+                    </Button>
+                    <input
+                        accept="image/*"
+                        className={classes.input}
+                        id=""
+                        multiple
+                        type="file"
                     />
                 </div>
             </Paper>
@@ -37,4 +62,6 @@ class HomePage extends PureComponent {
         );
     }
 }
-export default HomePage;
+
+
+export default withStyles(styles)(HomePage);
