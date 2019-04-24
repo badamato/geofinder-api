63c825882a2efa8349935ac81ba2c08ff2c2600e
Beth Damato <bethadamato@gmail.com>
2019-04-24 09:58:53 -0400

    adds styling

---
+++ ui/src/js/components/ReactMap.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ReactMap extends Component {
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
+}
+
+export default ReactMap;
